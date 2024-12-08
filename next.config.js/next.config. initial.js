d6e70769f845bf8c7e167aec0ea15c79ee41d806
QuantumForger <quantumaiforge@gmail.com>
2024-12-07 19:48:02 -0800

    gang

---
+++ next.config.js/next.config. initial.js
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+};
+
+module.exports = nextConfig;
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  webpack: (config) => {
+    config.watchOptions = {
+      ignored: ['**/node_modules', '**/.next', '**/*.log'],
+    };
+    return config;
+  },
+};
+
+module.exports = nextConfig;
